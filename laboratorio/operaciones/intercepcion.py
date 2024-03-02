dcfa246f60cf2bca7e20f6fcc34a4610a25ff486
juanDa-dev373 <127059670+juanDa-dev373@users.noreply.github.com>
2024-03-02 12:42:23 -0500

    Delete laboratorio directory

--- laboratorio/operaciones/intercepcion.py
+++
@@ -1,8 +0,0 @@
-def intercepcion2(conjunto1, conjunto2):
-    inter = []
-    if len(conjunto1)>len(conjunto2):
-        for i in range(len(conjunto1)):
-            if conjunto1[i]==conjunto2[i]:
-                inter[i] = conjunto1[i]
-
-    print()
